a56f9b552b10485ca2705b93a33ad5de65e28cde
Jasfiq Rahman <jasfiqrahman15@gmail.com>
2022-02-22 22:43:17 +0530

    Minor Edits

--- Assets/_Game/Scripts/Editor/Card/CardData/Effect Data/Effect Targeting/Effect Targeting Condition/EffectTargetingCondition_EditorExtension.cs
+++ Assets/_Game/Scripts/Editor/Card/CardData/Effect Data/Effect Targeting/Effect Targeting Condition/EffectTargetingCondition_EditorExtension.cs
@@ -1,16 +1,35 @@
 using DuelMasters.Card.Data.Effects.TargetingCondition.Parameters;
 using DuelMasters.Editor.Data.DataSetters;
+using UnityEditor;
+using UnityEngine;
 
 namespace DuelMasters.Editor.Data.Extensions
 {
     public static class EffectTargetingCondition_EditorExtension
     {
-        public static void AssignConditionParams(this EffectTargetingCondition condition)
+        public static void DrawInspector(this EffectTargetingCondition condition)
+        {
+            if (condition != null)
+            {
+                EditorGUILayout.LabelField("Targeting Condition:", EditorStyles.boldLabel);
+                GUILayout.BeginHorizontal();
+                GUILayout.Space(15);
+                GUILayout.BeginVertical();
+
+                AssignConditionParams(condition);
+                DrawConditionParamsLayout(condition);
+
+                GUILayout.EndVertical();
+                GUILayout.EndHorizontal();
+            }
+        }
+
+        private static void AssignConditionParams(EffectTargetingCondition condition)
         {
             EffectTargetingCondition_ParamSettingWrapper.AssignConditionParams(condition);
         }
 
-        public static void DrawConditionParamsLayout(this EffectTargetingCondition condition)
+        private static void DrawConditionParamsLayout(EffectTargetingCondition condition)
         {
             EffectTargetingCondition_ParamSettingWrapper.DrawConditionParamsLayout(condition);
         }
